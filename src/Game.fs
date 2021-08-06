f002964ec8f85fee6c27ae23079308672f64c49f
Theo Richart <theor@unity3d.com>
2021-08-06 14:29:00 +0200

    cards sprite sheet

--- src/Game.fs
+++ src/Game.fs
@@ -3,10 +3,21 @@ module Tarot.Game
 open Tarot.Types
 open Tarot.Ext
 
-type Player = { index: int; name: string; cards: Card[] }
+type Player = { Index: int; Name: string; Cards: Card[] }
 
-type Playing = { Players: Player []; Taker: int }
+type Trick = {
+    StartingPlayer: int
+    PlayedCards: Card list
+}
+type Playing = {
+    Players: Player []
+    Taker: int
+    Trick: Trick
+//    AttackerTricks: Card list
+//    DefenderTricks: Card list
+}
 with
+    member this.PlayerCount = this.Players.Length
     member this.TakerPlayer = this.Players.[this.Taker]
 
 type GameState =
@@ -16,14 +27,27 @@ type GameState =
     | Playing of Playing
 
 type Game = {
-    count: int
-    state: GameState
+    PlayerCount: int
+    State: GameState
 }
 
 let playCard (state:Playing) pi i =
-    let p = { state.Players.[pi] with cards =
-                        state.Players.[pi].cards |>  Seq.removeIndex i |> Seq.toArray
+    let p = { state.Players.[pi] with Cards =
+                        state.Players.[pi].Cards |>  Seq.removeIndex i |> Seq.toArray
             }
     { state with Players = state.Players
-                           |> Array.map (fun pp -> if pp.index <> p.index then pp else p )
+                           |> Array.map (fun pp -> if pp.Index <> p.Index then pp else p )
     }
+type PlayingState =
+    | WaitForCard of int
+    | EndRound
+    | EndGame
+let getPlayingState (s:Playing) =
+    match s.Trick with
+    | _ when s.Trick.PlayedCards.Length = s.PlayerCount ->
+        if s.Players.[0].Cards.Length = 0
+        then EndGame
+        else EndRound
+    | _ -> let nextPlayer = s.Trick.StartingPlayer + s.Trick.PlayedCards.Length
+           let nextPlayer = nextPlayer % s.Players.Length
+           WaitForCard nextPlayer
